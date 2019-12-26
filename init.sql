e67f6092bc8fe4c4a238bf4ccdc45dcf21200950
sigtot <sigurdtotland@gmail.com>
2019-12-26 23:25:49 +0100

    Add id to records table

--- init.sql
+++ init.sql
@@ -1,7 +1,11 @@
 CREATE DATABASE "notes";
 \c notes;
-CREATE TABLE IF NOT EXISTS record (
-    guess integer,
-    actual integer,
-    time time);
-INSERT INTO record (guess, actual, time) values (12, 13, NOW());
+CREATE TABLE IF NOT EXISTS record
+(
+    id     SERIAL PRIMARY KEY,
+    guess  INTEGER,
+    actual INTEGER,
+    time   TIME
+);
+INSERT INTO record (guess, actual, time)
+values (12, 13, NOW());
