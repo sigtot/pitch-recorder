c967c034fc5073340b604b9ff900df4a70ff8f6e
sigtot <sigurdtotland@gmail.com>
2019-12-27 17:08:35 +0100

    Make simple router structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Link, Route, BrowserRouter as Router, Switch} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/guess">Guess</Link>
+                    </li>
+                    <li>
+                        <Link to="/check">Check</Link>
+                    </li>
+                </ul>
+
+                <hr/>
+
+                {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+                <Switch>
+                    <Route exact path="/">
+                        <div>Home</div>
+                    </Route>
+                    <Route path="/guess">
+                        <div>Guess</div>
+                    </Route>
+                    <Route path="/check">
+                        <div>Check</div>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
