3094acfd3631030ae10b78cad3bae69047eb73be
sigtot <sigurdtotland@gmail.com>
2019-12-27 14:43:26 +0100

    Update swagger doc

--- src/hello_api/swagger.clj
+++ src/hello_api/swagger.clj
@@ -3,8 +3,7 @@
             [schema.core :as s]))
 
 (s/defschema Record {:guess  s/Int
-                     :actual s/Int
-                     :time   s/Int})
+                     :actual s/Int})
 
 (defn doc []
   (s/with-fn-validation
@@ -12,10 +11,13 @@
       {:info  {:version     "1.0.0"
                :title       "API Docs"
                :description "Docs for the API"}
-       :paths {"/records" {:get {:summary     "Records"
+       :paths {"/records" {:get {:summary     "Get records"
                                  :description "Show all records"
-                                 :parameters  {:path {:id s/Str}
-                                               :body Record}
                                  :responses   {200 {:schema      Record
                                                     :description "Everything works as expected"}
-                                               404 {:description "(Not implemented)"}}}}}})))
+                                               404 {:description "(Not implemented)"}}}
+                           :post {:summary    "Create record"
+                                 :description "Create a new record"
+                                 :parameters  {:body Record}
+                                 :responses   {200 {:schema      Record
+                                                    :description "Everything works as expected"}}}}}})))
