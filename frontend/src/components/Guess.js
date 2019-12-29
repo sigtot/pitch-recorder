1469004409c58b9a78452314646f09b55278f4bf
sigtot <sigurdtotland@gmail.com>
2019-12-29 14:38:56 +0100

    Implement GuessResult component

--- frontend/src/components/Guess.js
+++ frontend/src/components/Guess.js
@@ -1,13 +1,9 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
-import {keyName, getOctave} from '../piano'
 import KeyInput from './KeyInput'
 import NeuButton from "./NeuButton";
 import {Link, Route, Switch} from "react-router-dom";
-
-const Sub = styled.span`
-    font-size: 15px;
-`;
+import GuessResult from "./GuessResult";
 
 const GuessButton = styled(NeuButton)`
     font-size: 20px;
@@ -21,25 +17,49 @@ const GuessButtonContainer = styled.div`
     margin-top: 20px;
 `;
 
+const postGuess = (guess, actual) => {
+    console.log("Not implemented", guess, actual);
+};
+
 export default function Guess() {
     const [guess, setGuess] = useState(-1);
+    const [actual, setActual] = useState(-1);
     const [cNum, setCNum] = useState(3);
     return (
         <div>
-            <h1>{keyName(guess)}<Sub>({getOctave(guess) + 1})</Sub></h1>
+            <GuessResult guess={guess} actual={actual}/>
             <Switch>
                 <Route exact path="/guess">
                     <KeyInput
                         currentKey={guess} onKeyUpdate={key => setGuess(key)}
                         cNum={cNum} onCNumUpdate={cNum => setCNum(cNum)}/>
-                    <GuessButtonContainer>
-                        <Link to="/guess/check">
-                            <GuessButton>Make this guess</GuessButton>
-                        </Link>
-                    </GuessButtonContainer>
+                    {
+                        guess !== -1
+                            ? (
+                                <GuessButtonContainer>
+                                    <Link to="/guess/check">
+                                        <GuessButton>Make this guess</GuessButton>
+                                    </Link>
+                                </GuessButtonContainer>)
+                            : ''
+                    }
+
                 </Route>
                 <Route exact path="/guess/check">
-                    Check it
+                    <KeyInput
+                        currentKey={actual} onKeyUpdate={key => setActual(key)}
+                        cNum={cNum} onCNumUpdate={cNum => setCNum(cNum)}/>
+                    {
+                        actual !== -1 && guess !== -1
+                            ? (
+                                <GuessButtonContainer>
+                                    <GuessButton
+                                        onClick={postGuess(guess, actual)}>
+                                        Save this guess
+                                    </GuessButton>
+                                </GuessButtonContainer>)
+                            : ''
+                    }
                 </Route>
             </Switch>
         </div>
---
+++ frontend/src/components/GuessResult.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {keyName, getOctave} from '../piano';
+import styled from 'styled-components';
+
+const Sub = styled.span`
+    font-size: 15px;
+`;
+
+const TransitionDiv = styled.div`
+    transition: 0.2s;
+    display:flex;
+    justify-content: center;
+`;
+
+const KeyName = ({daKey}) => <span>{keyName(daKey)}<Sub>({getOctave(daKey) + 1})</Sub></span>;
+
+export default function GuessResult({guess, actual}) {
+    return (
+        <TransitionDiv>
+            <h1>
+                {guess !== -1 ? <KeyName daKey={guess}/> : ''}
+                {actual !== -1 ? <span>/<KeyName daKey={actual}/></span> : ''}
+            </h1>
+        </TransitionDiv>
+    )
+}
